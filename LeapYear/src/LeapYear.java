d2beafb6c27c9c1182239ccb40cc9b35ddf4a115
Aman <aman.kumar99499@gmail.com>
2022-08-08 09:05:23 +0530

    Added LeapYear

--- LeapYear/src/LeapYear.java
+++ LeapYear/src/LeapYear.java
@@ -7,9 +7,10 @@ public class LeapYear {
         System.out.println("To Check Find The Leap Year");
         System.out.println("Enter the Year");
         int a = scan.nextInt();
-        if((a % 400 == 0)){
+        if((a % 400 == 0) || (a%4 == 0)) {
             System.out.println("this " + a + " year is an Leap year");
         }
+        else{
         System.out.println("This is not an leap year");
     }
-}
+}}
