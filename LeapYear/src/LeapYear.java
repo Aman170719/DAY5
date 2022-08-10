89c847d19aed5a6c6c63995260243b0535527d83
Aman <aman.kumar99499@gmail.com>
2022-08-10 07:07:14 +0530

    Added BigNumber

--- LeapYear/src/LeapYear.java
+++ LeapYear/src/LeapYear.java
@@ -7,7 +7,7 @@ public class LeapYear {
         System.out.println("To Check Find The Leap Year");
         System.out.println("Enter the Year");
         int a = scan.nextInt();
-        if((a % 400 == 0)){
+        if((a % 400 == 0)||( a % 4== 0)||(a%100 != 0)){
             System.out.println("this " + a + " year is an Leap year");
         }
         System.out.println("This is not an leap year");
